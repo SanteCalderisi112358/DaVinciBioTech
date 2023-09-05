99eee9e4f8089174a69cec822ca0ba5ef331a510
SanteCalderisi112358 <santecalderisi@gmail.com>
2023-09-05 10:33:32 +0200

    Creazione Progetto 'DaVinciBioTech'

---
+++ DaVinciBioTechBE/src/main/java/com/davincibiotech/DaVinciBioTechBE/DaVinciBioTechBeApplication.java
@@ -0,0 +1,13 @@
+package com.davincibiotech.DaVinciBioTechBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaVinciBioTechBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaVinciBioTechBeApplication.class, args);
+	}
+
+}
---
+++ DaVinciBioTechBE/src/test/java/com/davincibiotech/DaVinciBioTechBE/DaVinciBioTechBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davincibiotech.DaVinciBioTechBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaVinciBioTechBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
