fcb69eca20afe30246fb3ad462a3756aca47af79
SanteCalderisi112358 <santecalderisi@gmail.com>
2023-09-20 11:32:18 +0200

    Home con biografia completa

--- DaVinciBioTechFE/DaVinciBioTechFE/src/app/components/home/home.component.ts
+++ DaVinciBioTechFE/DaVinciBioTechFE/src/app/components/home/home.component.ts
@@ -22,6 +22,8 @@ utente!: Utente;
   showSearchInput: boolean = false;
   tavolaCasuale: Tavola | undefined;
   isLoading:boolean = true;
+  anno:string | undefined;
+  scrollTop: number = 0;
    constructor(private dvbtSrv: DvbtService, private authSrv: AuthService) { }
 
 
@@ -38,7 +40,30 @@ utente!: Utente;
 
   }
 
-
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    this.scrollTop = window.scrollY;
+    console.log(this.scrollTop)
+    if(window.scrollY>0 && window.scrollY<900){
+      this.anno = '1452'
+    }else if(window.scrollY>1000 && window.scrollY<1100){
+      this.anno = '1466'
+    }else if(window.scrollY>1200 && window.scrollY<1280){
+      this.anno = '1482'
+    }else if(window.scrollY>1344 && window.scrollY<1436){
+      this.anno = '1498'
+    }else if(window.scrollY>1523 && window.scrollY<1624){
+      this.anno = '1503'
+    }else if(window.scrollY>1712 && window.scrollY<1837){
+      this.anno = '1513'
+    }else if(window.scrollY>1896 && window.scrollY<2000){
+      this.anno = '1516'
+    }else if(window.scrollY>2200 && window.scrollY<2325){
+      this.anno = '1570'
+    }else if(window.scrollY>2400){
+      this.anno = 'Oggi'
+    }
+  }
 
 }
 
